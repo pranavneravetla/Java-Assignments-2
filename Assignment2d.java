60c4b2a42eaaea29cfcfd89b33aa9a6141900cfd
pranavneravetla <pranavneravetla@gmail.com>
2016-06-14 12:18:21 +0530

    Add files via upload

---
+++ Assignment2d.java
@@ -0,0 +1,19 @@
+
+public class Assignment2d {
+
+	public static void main(String[] args) {
+		int x=5;
+		int y=10;
+
+		System.out.println(x + y * 2);
+		System.out.println(x - y * 2);
+		System.out.println((x + y) * 2);
+		System.out.println(y % x);
+
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
